fb77c63c875ba15a45b515890b1693ac86f33f8a
José Augusto Mello Crispim <101072311+AugustoMello09@users.noreply.github.com>
2023-09-04 11:25:13 -0300

    feat: Home e variavel

--- LocadoraFront/src/app/app.module.ts
+++ LocadoraFront/src/app/app.module.ts
@@ -3,17 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
-import {InputTextModule} from 'primeng/inputtext';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    InputTextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
