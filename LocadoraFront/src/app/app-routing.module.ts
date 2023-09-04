4aa78e22362378ecd72a1268f7197ba2ce45b61d
José Augusto Mello Crispim <101072311+AugustoMello09@users.noreply.github.com>
2023-09-04 15:32:45 -0300

    feat: Inicio

--- LocadoraFront/src/app/app-routing.module.ts
+++ LocadoraFront/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { InicioComponent } from './telas/inicio/inicio.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent }
+  {path: '', component: HomeComponent },
+  {path: 'inicio', component: InicioComponent }
 ];
 
 @NgModule({
--- LocadoraFront/src/app/app.module.ts
+++ LocadoraFront/src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { InicioComponent } from './telas/inicio/inicio.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
