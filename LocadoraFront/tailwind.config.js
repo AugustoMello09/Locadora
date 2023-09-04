7535b0ac980fb74f290887a3b408f8f4db656dd5
José Augusto Mello Crispim <101072311+AugustoMello09@users.noreply.github.com>
2023-09-04 10:20:36 -0300

    feat: Adicionando variavel

--- LocadoraFront/tailwind.config.js
+++ LocadoraFront/tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-fundo' : '#146C94',
+        'custom-bloco' : '#1685A4',
+        'custom-botao' : '#00425A',
+        'custom-span' : '#AFD3E2',
+        'custom-font-destaque' : '#FFFFFF',
+        'custom-font-secundario' : '#F6F1F1'
+      },
+      fontFamily: {
+        'font-primaria': ['Roboto', 'sans-serif'],
+        'font-secundaria': ['Poppins', 'sans-serif'],
+      },
+      screens: {
+        'auto': {'max' : '1980px'},
+        'desktop': { 'max': '1280px' },
+        'laptop': {'max': '1024px'},
+        'tablet': {'max': '640px'},
+        'sm': {'max': '360px'},
+      },
+    },
   },
   plugins: [],
 }
