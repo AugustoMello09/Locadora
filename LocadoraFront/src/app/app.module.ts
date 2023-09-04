a5ecbf340cae74ae8f39f2f10cbbeeb4e6e1188c
José Augusto Mello Crispim <101072311+AugustoMello09@users.noreply.github.com>
2023-09-04 09:59:35 -0300

    feat: Adicionando a biblioteca priming

--- LocadoraFront/src/app/app.module.ts
+++ LocadoraFront/src/app/app.module.ts
@@ -4,13 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {InputTextModule} from 'primeng/inputtext';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    InputTextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
