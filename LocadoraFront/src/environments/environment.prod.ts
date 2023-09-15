8595a7d9bb29d3842a68e24de862300863f890d7
José Augusto Mello Crispim <101072311+AugustoMello09@users.noreply.github.com>
2023-09-15 14:06:41 -0300

    Dockerfile

--- LocadoraFront/src/environments/environment.prod.ts
+++ LocadoraFront/src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: 'http://localhost:8080',
+  clientId: 'mylocadora',
+  clientSecret: 'locadora123',
+  obterTokenUrl: '/oauth/token'
 };
